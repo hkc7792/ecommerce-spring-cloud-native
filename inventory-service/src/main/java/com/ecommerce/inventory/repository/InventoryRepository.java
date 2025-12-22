7a897a5a3f1a6a18e89cb620d562dea3393824ec
hkc7792 <hkc7792@gmail.com>
2025-12-23 01:16:03 +0530

    add db layer

---
+++ inventory-service/src/main/java/com/ecommerce/inventory/repository/InventoryRepository.java
@@ -0,0 +1,11 @@
+package com.ecommerce.inventory.repository;
+
+import com.ecommerce.inventory.entites.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
+
+}
